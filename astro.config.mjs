83e10e0075dfbca57db8e132f3abd9d278b784ed
franlo42 <franlozmell@gmail.com>
2025-02-18 14:34:24 +0100

    Added: Github Actions to automate deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+
+  site: 'https://franlo42.is-a.dev',
+
   devToolbar: {
     enabled: false
   },
